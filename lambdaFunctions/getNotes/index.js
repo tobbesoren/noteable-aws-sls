2c363dbe4c8bdb0deeeb85f2c47a05fe109a568e
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-11-24 11:31:37 +0100

    getNotesUpdatedToOnlyGetCurrentUsersNotes

--- lambdaFunctions/getNotes/index.js
+++ lambdaFunctions/getNotes/index.js
@@ -9,16 +9,24 @@ const getNotes = async (event, context) => {
   if (event?.error && event.error === "401") {
     return sendResponse(401, { success: false, message: "Invalid token" });
   }
+  const userId = event.userId;
 
   try {
     const { Items } = await db
       .scan({
         TableName: "noteableNotes",
+        FilterExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+          ":userId": userId,
+        },
       })
       .promise();
     return sendResponse(200, { success: true, notes: Items });
   } catch (error) {
-    return sendResponse(400, { success: false, message: "Could not get notes" });
+    return sendResponse(400, {
+      success: false,
+      message: "Could not get notes",
+    });
   }
 };
 
